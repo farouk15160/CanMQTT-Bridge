20db84b1874a2938dfe83dffe566886e71f93cd4
Farouk <farouk15160@gmail.com>
2025-06-10 21:51:04 +0200

    Update clock.go

--- internal/bridge/clock.go
+++ internal/bridge/clock.go
@@ -60,6 +60,23 @@ func runClockSender(wg *sync.WaitGroup) {
 				log.Printf("Clock Sender: Sent time %d (ID: %X, Len: %d)", nowNano, frame.ID, frame.Length)
 			}
 			// Error is already logged by canPublish if it occurs
+			// Schedule delayed frame
+			time.AfterFunc(time.Duration(targetDelayMs)*time.Millisecond, func() {
+				nowNanoDelayed := time.Now().UnixNano()
+				var data [8]byte
+				binary.LittleEndian.PutUint64(data[:], uint64(nowNanoDelayed))
+
+				frame := can.Frame{
+					ID:     0x6,
+					Length: 8,
+					Data:   data,
+				}
+
+				err := canPublish(frame)
+				if err == nil && IsDebugEnabled() {
+					log.Printf("Clock drive Sender: Sent time %d (ID: %X, Len: %d)", nowNanoDelayed, frame.ID, frame.Length)
+				}
+			})
 
 		} // end select
 	} // end for
